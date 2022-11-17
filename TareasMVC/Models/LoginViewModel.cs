b4aee3c993f845f94858098c6f3d6642b4d958a5
JoanSerratoFlores <joan.serrato.flores@gmail.com>
2022-11-17 00:39:56 -0600

    Login

---
+++ TareasMVC/Models/LoginViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace TareasMVC.Models
+{
+    public class LoginViewModel
+    {
+        [Required(ErrorMessage = "EL campo {0} es requerido")]
+        [EmailAddress(ErrorMessage = "EL campo debe ser un correo electronico valido")]
+        public string Email { get; set; }
+        [Required(ErrorMessage = "EL campo {0} es requerido")]
+        [DataType(DataType.Password)]
+        public string Password { get; set; }
+        [Display(Name = "Recuérdame")]        
+        public bool Recuerdame { get; set; }
+    }
+}
