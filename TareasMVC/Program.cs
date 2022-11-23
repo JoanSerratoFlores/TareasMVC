06a85e12919f4921b9ff7aebd86da5abe02da02d
JoanSerratoFlores <joan.serrato.flores@gmail.com>
2022-11-23 00:44:03 -0600

    Tareas en Base de Datos

--- TareasMVC/Program.cs
+++ TareasMVC/Program.cs
@@ -52,6 +52,9 @@ builder.Services.AddLocalization(opciones =>
     opciones.ResourcesPath = "Recursos";
 });
 
+builder.Services.AddTransient<IServicioUsuarios, ServicioUsuarios>();
+builder.Services.AddAutoMapper(typeof(Program));
+
 var app = builder.Build();
 
 app.UseRequestLocalization(opciones =>
--- TareasMVC/wwwroot/js/Tareas.js
+++ TareasMVC/wwwroot/js/Tareas.js
@@ -5,11 +5,50 @@
 
 }
 
-function manejarFocusoutTituloTarea(tarea) {
+async function manejarFocusoutTituloTarea(tarea) {
     const titulo = tarea.titulo();
     if (!titulo) {
         tareaListadoViewModel.tareas.pop();
         return;
     }
-    tarea.id(1);
+
+    const data = JSON.stringify(titulo);
+    const respuesta = await fetch(urlTareas, {
+        method: 'POST',
+        body: data,
+        headers: {
+            'Content-Type': 'application/json'
+        }
+    });
+
+    if (respuesta.ok) {
+        const json = await respuesta.json();
+        tarea.id(json.id);
+    } else {
+        //Mostrar mensaje de error  
+    }
+}
+
+async function obtenerTareas() {
+    tareaListadoViewModel.cargando(true);
+
+    const respuesta = await fetch(urlTareas, {
+        method: 'GET',
+        headers: {
+            'Content-Type':'application/json'
+        }
+    })
+
+    if (!respuesta.ok) {
+        return;
+    }
+
+    const json = await respuesta.json();
+    tareaListadoViewModel.tareas([]);
+
+    json.forEach(valor => {
+        tareaListadoViewModel.tareas.push(new tareaElementoListadoViewModel(valor));
+    });
+
+    tareaListadoViewModel.cargando(false);
 }
